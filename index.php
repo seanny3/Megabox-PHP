d7fe79c654d9f80d2fd893fb829fd50defe0e2e8
seanny3 <dev.seann@gmail.com>
2022-01-11 12:00:00 +0900

    test_upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+    <title>MEET PLAY SHARE, 메가박스</title>
+</head>
+<body>
+    <div id="bg_black">
+        <div class="wrap">
+            <header>
+                <?php include("/header.php"); ?>
+            </header>
+            <section id="boxoffice">
+                
+            </section>
+        </div>
+    </div>
+        <section id="benefit">
+
+        </section>
+        <section id="curation">
+
+        </section>
+        <section id="information">
+
+        </section>
+        <section id="banner">
+
+        </section>
+        <footer>
+            <?php include("/footer.php"); ?>
+        </footer>
+</body>
+</html>
