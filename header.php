c4e7c972f67e670a7ed7eed7d4c2af17a2379902
seanny3 <dev.seann@gmail.com>
2022-01-12 12:00:00 +0900

    헤더, 메뉴 구현

---
+++ header.php
@@ -0,0 +1,68 @@
+<div id="first-line">
+    <span id="left-link">
+        <a href="" title="VIP LOUNGE">VIP LOUNGE</a>
+        <a href="" title="멤버십">멤버십</a>
+        <a href="" title="고객센터">고객센터</a>
+    </span>
+    <span id="right-link">
+        <a href="" title="로그인">로그인</a>
+        <a href="" title="회원가입">회원가입</a>
+        <a href="" title="빠른예매">빠른예매</a>
+    </span>
+</div>
+<div id="second-line">
+    <div id="link-icon">
+        <a href="" title="사이트맵"></a>
+        <a href="" title="검색"></a>
+        <a href="" title="상영시간표"></a>
+        <a href="" title="나의 메가박스"></a>
+    </div>
+    <nav id="categories">
+        <ul>
+            <li>
+                <a href="" title="영화">영화</a>
+                <div class="sub_menu">
+                    <a href="" title="전체영화">전체영화</a>
+                    <a href="" title="큐레이션">큐레이션</a>
+                    <a href="" title="무비포스트">무비포스트</a>
+                </div>
+            </li>
+            <li>
+                <a href="" title="예매">예매</a>
+                <div class="sub_menu">
+                    <a href="" title="빠른예매">빠른예매</a>
+                    <a href="" title="상영시간표">상영시간표</a>
+                    <a href="" title="더 부티크 프라이빗 예매">더 부티크 프라이빗 예매</a>
+                </div>
+            </li>
+            <li>
+                <a href="" title="극장">극장</a>
+                <div class="sub_menu">
+                    <a href="" title="전체극장">전체극장</a>
+                    <a href="" title="특별관">특별관</a>
+                </div>
+            </li>
+            <li>
+                <a href="" title="이벤트">이벤트</a>
+                <div class="sub_menu">
+                    <a href="" title="진행중 이벤트">진행중 이벤트</a>
+                    <a href="" title="지난 이벤트">지난 이벤트</a>
+                    <a href="" title="당첨자발표">당첨자발표</a>
+                </div>
+            </li>
+            <li>
+                <a href="" title="스토어">스토어</a>
+            </li>
+            <li>
+                <a href="" title="혜택">혜택</a>
+                <div class="sub_menu">
+                    <a href="" title="메가박스 멤버십">메가박스 멤버십</a>
+                    <a href="" title="제휴/할인">제휴/할인</a>
+                </div>
+            </li>
+        </ul>
+    </nav>
+</div>
+<div id="logo">
+    <a href="/" title="MEGABOX 메인으로 가기"></a>
+</div>
--- index.php
+++ index.php
@@ -5,33 +5,68 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/common.css">
     <title>MEET PLAY SHARE, 메가박스</title>
 </head>
 <body>
-    <div id="bg_black">
+    <header class="main_bg">
         <div class="wrap">
-            <header>
-                <?php include("/header.php"); ?>
-            </header>
+            <?php include("./header.php"); ?>
+        </div>
+    </header>
+    <section id="main">
+        <div id="bg">
+            <div id="bg_pattern"></div>
+            <img src="/img/main/bg_img.jpg" alt="">
+        </div>
+        <div class="wrap">
             <section id="boxoffice">
-                
+                <div id="boxoffice_title">
+                    <h5>박스오피스</h5>
+                    <a href="">더 많은 영화보기 +</a>
+                </div>
+                <article id="boxoffice_rank">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </article>
+                <ul id="search-link">
+                    <li>
+                        <form action="" method="post">
+                            <input type="text" placehold="영화명을 입력해 주세요">
+                            <button type="submit"></button>
+                        </form>
+                    </li>
+                    <li>
+                        <a href="" title="상영시간표 보기">상영시간표</a>
+                    </li>
+                    <li>
+                        <a href="" title="박스오피스 보기">박스오피스</a>
+                    </li>
+                    <li>
+                        <a href="" title="빠른예매 보기">빠른예매</a>
+                    </li>
+                </ul>
             </section>
         </div>
-    </div>
-        <section id="benefit">
+    </section>
+    <section id="benefit">
 
-        </section>
-        <section id="curation">
+    </section>
+    <section id="curation">
 
-        </section>
-        <section id="information">
+    </section>
+    <section id="information">
 
-        </section>
-        <section id="banner">
+    </section>
+    <section id="banner">
 
-        </section>
-        <footer>
-            <?php include("/footer.php"); ?>
-        </footer>
+    </section>
+    <footer>
+        <?php include("./footer.php"); ?>
+    </footer>
 </body>
 </html>
