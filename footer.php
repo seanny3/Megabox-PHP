ebf3f8e0d749df2ff7535d1366a557499464dd31
seanny3 <dev.seann@gmail.com>
2022-01-14 12:00:00 +0900

    main benefits section completion, slider X | foooter completion

---
+++ footer.php
@@ -0,0 +1,27 @@
+<div id="footer_top">
+    <dl>
+        <dd>회사소개</dd>
+        <dd>인재채용</dd>
+        <dd>사회공헌</dd>
+        <dd>제휴/광고/부대사업문의</dd>
+        <dd>이용약관</dd>
+        <dd>위치기반서비스 이용약관</dd>
+        <dd><b>개인정보처리방침</b></dd>
+        <dd>윤리경영</dd>
+    </dl>
+    <a href="" title="극장찾기">극장찾기</a>
+</div>
+<div id="footer_container">
+    <img src="/img/footer/footer_logo.png" alt="">
+    <div id="footer_info">
+        <p>서울특별시 마포구 월드컵로 240, 지상2층(성산동, 월드컵주경기장) ARS 1544-0070</p>
+        <p>대표자명 홍정인 · 개인정보보호책임자 공성진 · 사업자등록번호 211-86-59478 · 통신판매업신고번호 제 2020-서울마포-4545 호</p>
+        <p>COPYRIGHT © MegaboxJoongAng, Inc. All rights reserved</p>
+    </div>
+    <div id="footer_sns">
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -76,20 +76,87 @@
             </div>
         </div>
     </section>
-    <section id="benefit">
+    <section id="benefits">
+        <div class="wrap">
+            <div class="section_title">
+                <h2>혜택</h2>
+                <a href=""></a>
+            </div>
+            <article id="benefits_ad">
+                <div id="benefits_slider">
 
+                </div>
+                <div id="benefits_banner">
+                    <img src="/img/main/benefits_banner_1.jpg" alt="">
+                    <img src="/img/main/benefits_banner_2.jpg" alt="">
+                </div>
+                <img src="/img/main/benefits_banner_3.jpg" alt="">
+            </article>
+            <ul id="menu_links">
+                <li><a href="" title="VIP LOUNGE 페이지로 이동">VIP LOUNGE</a></li>
+                <li><a href="" title="멤버십 페이지로 이동">멤버십</a></li>
+                <li><a href="" title="할인카드안내 페이지로 이동">할인카드안내</a></li>
+                <li><a href="" title="이벤트 페이지로 이동">이벤트</a></li>
+                <li><a href="" title="스토어 페이지로 이동">스토어</a></li>
+            </ul>
+        </div>
     </section>
     <section id="curation">
+        <div class="wrap">
+            <div class="section_title">
+                <h2>큐레이션</h2>
+                <a href="">큐레이션 더보기</a>
+            </div>
+            <article id="curation_container">
 
+            </article>
+        </div>
     </section>
     <section id="information">
-
+        <div class="wrap">
+            <div class="section_title">
+                <h2>메가박스 안내</h2>
+            </div>
+            <ul id="info-special_list">
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </div>
+        <div id="info_notice">
+            <div class="wrap">
+                <dl>
+                    <dt>지점</dt>
+                    <dd>
+                        <span>[남양주현대아울렛 스페이스원] 영화 관람요금 기준 변경 안내</span>
+                        <span>2022.04.28</span>
+                    </dd>
+                    <a href="" title="전체공지 더보기">더보기</a>
+                </dl>
+            </div>
+        </div>
+        <div class="wrap">
+            <ul id="info_links">
+                <li><a href="">고객센터</a></li>
+                <li><a href="">자주 묻는 질문</a></li>
+                <li><a href="">1:1 문의</a></li>
+                <li><a href="">단체/대관문의</a></li>
+                <li><a href="">분실물 문의/접수</a></li>
+                <li><a href="">더 부티크 프라이빗<br />대관예매</a></li>
+            </ul>
+        </div>
     </section>
     <section id="banner">
-
+        <div class="wrap">
+            <img src="/img/main/footer_banner.jpg" alt="">
+        </div>
     </section>
     <footer>
-        <?php include("./footer.php"); ?>
+        <div class="wrap">
+            <?php include("./footer.php"); ?>
+        </div>
     </footer>
 </body>
 </html>
